e3d774a2d71e008e5ed7d427519b7be62c6f8730
Oliver <oliver.drungell@gmail.com>
2024-10-26 12:01:28 -0500

    Script folder

---
+++ rowdyhacks-fall24/Assets/Scripts/Scrolling.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scrolling : MonoBehaviour
+{
+    public float scrollSpeed = 10.5f;
+    
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
